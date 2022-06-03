f9090421fa8596c8df48a5de126f49e638db76a3
JSnik <js.saba999@gmail.com>
2022-06-03 18:30:04 +0400

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   isThemeChanged: boolean = false;
+  stacking: boolean = false;
+
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+    if (this.route._routerState.url === '/stacking') {
+      console.log('ssss')
+      this.stacking = true;
+    }
+  }
 
   changeTheme(value: boolean) {
     this.isThemeChanged = value;
