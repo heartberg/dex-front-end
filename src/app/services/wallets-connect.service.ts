492ee79b3e0a5e279de78f80d22a3f019cb86815
JSnik <js.saba999@gmail.com>
2022-06-29 17:03:12 +0400

    ..

--- src/app/services/wallets-connect.service.ts
+++ src/app/services/wallets-connect.service.ts
@@ -13,6 +13,7 @@ import MyAlgoConnect from '@randlabs/myalgo-connect';
 import { Buffer } from 'buffer';
 import { PermissionResult, SessionWallet, SignedTxn, allowedWallets, PermissionCallback } from 'algorand-session-wallet';
 import {Cookie} from "ng2-cookies";
+import {Router} from "@angular/router";
 
 
 const client = getAlgodClient()
@@ -26,14 +27,12 @@ export class WalletsConnectService {
   public myAlgoAddress: any | undefined;
   public myAlgoName: any | undefined;
 
-  constructor(private userServce: AuthService) {
+  constructor(private userServce: AuthService, private router: Router) {
     if (localStorage.getItem('wallet')) {
       sessionStorage.setItem('acct-list', JSON.stringify([localStorage.getItem('wallet')]));
-      setTimeout(() => {
         if (this.sessionWallet === undefined || !this.sessionWallet) {
           this.connectOnDefault('my-algo-connect').then(response => response);
         }
-      })
     }
   }
 
@@ -62,10 +61,13 @@ export class WalletsConnectService {
       location.reload();
       setTimeout(() => {
         localStorage.removeItem('reload');
-      }, 300)
+      }, 1000)
     } else {
       return
     }
+    // this.router.navigateByUrl('/trade', { skipLocationChange: true }).then(() => {
+    //   this.router.navigate(['trade']);
+    // });
   }
 
   connectOnDefault = async (choice: string) => {
